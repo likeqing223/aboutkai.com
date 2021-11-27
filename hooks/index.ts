a39e1a130d095fe3bbdec77014496cd72ca26b07
kaichi <hellokitty321ds@gmail.com>
2021-11-27 22:20:05 +0800

    clean code

--- hooks/index.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo, useState } from "react";
-
-export function usePagination(page = 5) {
-  const [index, setIndex] = useState(0);
-  const start = useMemo(() => index * page, [page, index]);
-  const end = useMemo(() => start + page, [start, page]);
-
-  return [start, end, setIndex] as const;
-}
