eb8bde32d6b35d9812e4acac518ee3d93539a770
kaichi <hellokitty321ds@gmail.com>
2021-12-21 01:11:08 +0800

    stash

--- components/MDWrapper.tsx
+++ components/MDWrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function MDWrapper(props: PropsWithChildren<{}>) {
   return (
-    <div className="w-full prose prose-gray dark:prose-dark max-w-none">
+    <div className="w-full prose dark:prose-invert max-w-none">
       {props.children}
     </div>
   );
