3e1be6aa2f810d85413a959d3a95dd2d942f0a68
kaichi <hellokitty321ds@gmail.com>
2022-02-18 13:45:57 +0800

    finished

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -26,16 +26,14 @@ export default function Blog({
   return (
     <Container
       title="博客 - kaichi"
-      description="在学习的过程中，优质的知识分享博客是我能够成长的主要输入来源，非常感谢认真做知识分享的博主。在分享中传播知识的同时有利于知识的巩固，所以我决定记录下我的学习历程。包括
-      JavaScript & TypeScript、 React、Rust、Flutter、GraphQL..."
+      description="写博客对于知识的梳理、理解的加深有很大帮助，有时也会发现自己理解错误的地方。另外，也可以帮助到其他需要这些知识的人，希望我的博客对你有所帮助。"
     >
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-8">
         <h1 className="mb-4 text-2xl md:text-3xl font-medium tracking-tight">
           博客
         </h1>
         <p className="mb-6 text-gray-700 dark:text-gray-500">
-          在学习的过程中，优质的知识分享博客是我能够成长的主要输入来源，非常感谢认真做知识分享的博主。在分享中传播知识的同时有利于知识的巩固，所以我决定记录下我的学习历程。包括
-          JavaScript & TypeScript、 React、GraphQL、Flutter...
+          写博客对于知识的梳理、理解的加深有很大帮助，有时也会发现自己理解错误的地方。另外，也可以帮助到其他需要这些知识的人，希望我的博客对你有所帮助。
         </p>
         <div className="relative w-full mb-6">
           <input
