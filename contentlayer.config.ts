d9d2252870c6e7b0a0d69aca1e6cce66d02d1ec5
kaichi <hellokitty321ds@gmail.com>
2022-06-14 15:04:43 +0800

    fix: hast type

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,47 +2,47 @@ import {
   defineDocumentType,
   makeSource,
   ComputedFields
-} from "contentlayer/source-files";
-import readingTime from "reading-time";
-import remarkGfm from "remark-gfm";
-import rehypeSlug from "rehype-slug";
-import rehypeCodeTitles from "rehype-code-titles";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrism from "rehype-prism-plus";
-import { Element } from "hast";
-import rehypeUrls from "rehype-urls";
-import pkg from "./package.json";
+} from 'contentlayer/source-files';
+import readingTime from 'reading-time';
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeCodeTitles from 'rehype-code-titles';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypePrism from 'rehype-prism-plus';
+import type { Element } from 'hast';
+import rehypeUrls from 'rehype-urls';
+import pkg from './package.json';
 
 const computedFields: ComputedFields = {
   readingTime: {
-    type: "number",
+    type: 'number',
     resolve: (doc) => readingTime(doc.body.raw).minutes
   },
   wordCount: {
-    type: "number",
+    type: 'number',
     resolve: (doc) => doc.body.raw.split(/\s+/gu).length
   },
   slug: {
-    type: "string",
-    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    type: 'string',
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, '')
   }
 };
 
 const Blog = defineDocumentType(() => ({
-  name: "Blog",
-  filePathPattern: "blog/*.mdx",
-  bodyType: "mdx",
+  name: 'Blog',
+  filePathPattern: 'blog/*.mdx',
+  bodyType: 'mdx',
   fields: {
-    title: { type: "string", required: true },
-    description: { type: "string", required: true },
-    logo: { type: "string" },
-    publishedAt: { type: "string", required: true }
+    title: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    logo: { type: 'string' },
+    publishedAt: { type: 'string', required: true }
   },
   computedFields
 }));
 
 const contentLayerConfig = makeSource({
-  contentDirPath: "data",
+  contentDirPath: 'data',
   documentTypes: [Blog],
   mdx: {
     remarkPlugins: [remarkGfm],
@@ -55,7 +55,7 @@ const contentLayerConfig = makeSource({
         {
           test: [`h2`, `h3`, `h4`, `h5`, `h6`],
           properties: {
-            className: ["anchor"]
+            className: ['anchor']
           }
         }
       ],
@@ -63,14 +63,14 @@ const contentLayerConfig = makeSource({
         rehypeUrls,
         (url: URL, node: Element) => {
           const isInternalLink =
-            url.href.startsWith("/") ||
-            url.href.startsWith("#") ||
+            url.href.startsWith('/') ||
+            url.href.startsWith('#') ||
             url.host === new URL(pkg.homepage).host;
 
           if (!isInternalLink) {
             if (node.properties) {
-              node.properties.target = "_blank";
-              node.properties.rel = "noopener noreferrer";
+              node.properties.target = '_blank';
+              node.properties.rel = 'noopener noreferrer';
             }
           }
         }
