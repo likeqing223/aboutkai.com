f48c7bf879446cef20449d49436b09428c300df4
kaichi <hellokitty321ds@gmail.com>
2022-06-21 16:07:03 +0800

    add progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
 import 'styles/global.css';
+import 'styles/nprogress.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
